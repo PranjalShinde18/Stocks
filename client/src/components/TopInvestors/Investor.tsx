c640db1906dc5da43dbb0cbadd4e742888c3e025
Saksham <113179290+Saksham131102@users.noreply.github.com>
2024-08-16 00:48:01 +0530

    Search filter functionality added. cards modified

---
+++ client/src/components/TopInvestors/Investor.tsx
@@ -0,0 +1,33 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { FaArrowRight } from "react-icons/fa6";
+
+interface Props {
+  name: string;
+  description: string;
+}
+
+const Investor = ({ name, description }: Props) => {
+  return (
+    <div
+      className={`flex flex-col items-center overflow-hidden group
+                      w-full sm:w-[calc(50%-1rem)] md:w-[calc(33.33%-1rem)] lg:w-[calc(25%-1rem)]
+                      h-[280px] sm:h-64 md:h-72 lg:h-[340px] p-4 border rounded-xl shadow-md shadow-gray-400 
+                      transition-all hover:shadow-lg hover:shadow-gray-400`}
+    >
+      <Avatar className="w-28 h-28 sm:w-20 sm:h-20 md:w-28 md:h-28 lg:w-40 lg:h-40">
+        <AvatarImage src="https://github.com/shadcn.png" />
+        <AvatarFallback>CN</AvatarFallback>
+      </Avatar>
+
+      <div className="font-semibold w-full mt-4 mb-2 text-left">{name}</div>
+      <div className="text-sm sm:text-base text-left line-clamp-2 w-full">
+        {description}
+      </div>
+      <div className="w-full flex justify-end items-center mt-4 pr-2 transition-all group-hover:pr-0">
+        <FaArrowRight className="text-lg" />
+      </div>
+    </div>
+  );
+};
+
+export default Investor;
---
+++ client/src/investors/index.ts
@@ -0,0 +1,34 @@
+const investors = [
+  {
+    name: "Sunil Singhania",
+    description:
+      "Sunil Singhania is a CFA who founded Abakkus AMC in 2018. His investment strategy includes well-diversified portfolio, investment in large as well as small firms and investment in companies with enormous potential. These are the shares held by Sunil Singhania as per the information available by the exchanges. For some companies the latest quarter results might not be available as they may file it later on.",
+  },
+  {
+    name: "Anil Kumar Goel",
+    description:
+      "He started investing in the 90s. His investment strategy includes investing in small and micro-scale firms. These are the shares held by Anil Kumar Goel as per the information available by the exchanges. For some companies the latest quarter results might not be available as they may file it later on.",
+  },
+  {
+    name: "Ashish Dhawan",
+    description:
+      "Ashish is one of the successful investors in India. In 1999 he founded ChrysCapital along with Raj Kondur, which later became one of India's biggest funds in India. His investment strategy includes smart and diversified investment with proper market analysis for long term. These are the shares held by Ashish Dhawan as per the information available by the exchanges. For some companies the latest quarter results might not be available as they may file it later on.",
+  },
+  {
+    name: "Ashish Kacholia",
+    description:
+      "Ashish Kacholia is an ace investor. He co-founded Hungama Digital with Rakesh Jhunjhunwala and started his own company Lucky Securities in 2003. His investment strategy includes investing in small and mid-size companies. These are the shares held by Ashish Kacholia as per the information available by the exchanges. For some companies the latest quarter results might not be available as they may file it later on.",
+  },
+  {
+    name: "Dilipkumar Lakhi",
+    description:
+      "Dilip is one of the most successful diamond merchants and investors in India. His investment strategy includes investing in less popular segments like lubricants, avoiding popular segments like banks, automobiles, investing in the least competitive segments, relying on reversals and contra calls. These are the shares held by Dilipkumar Lakhi as per the information available by the exchanges. For some companies the latest quarter results might not be available as they may file it later on.",
+  },
+  {
+    name: "Dolly Rajiv Khanna",
+    description:
+      "Dolly Khana is a large investor who works with her husband in creating a most successful portfolio. She has been investing since 1996. Her investment strategy includes patience, conducting proper research, special attention to stop-loss, knowing about the company before investing, and investing in few companies. These are the shares held by Dolly Khana as per the information available by the exchanges. For some companies the latest quarter results might not be available as they may file it later on.",
+  },
+];
+
+export default investors;
