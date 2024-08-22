7e166bc4e7eae17e185b4782df4c85aeff5d40fd
Saksham <113179290+Saksham131102@users.noreply.github.com>
2024-08-22 21:47:54 +0530

    started backend

--- server/server.js
+++ server/server.js
@@ -1,16 +1,14 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
 
 // Connect Database
-const db = require('./config/db.js');
+const db = require("./config/db.js");
 
 app.use(cors());
 
-
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 6000;
 
 app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`)
-}
-);
+  console.log(`Server started on port ${PORT}`);
+});
