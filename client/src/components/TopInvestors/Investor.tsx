320c4bcb716c0688815d593284843a78822a53f4
Saksham <113179290+Saksham131102@users.noreply.github.com>
2024-08-23 18:43:45 +0530

    added border in images

--- client/src/components/TopInvestors/Investor.tsx
+++ client/src/components/TopInvestors/Investor.tsx
@@ -15,12 +15,14 @@ const Investor = ({ name, image_url, description }: Props) => {
                       h-[280px] sm:h-64 md:h-72 lg:h-[340px] p-4 border rounded-xl shadow-md shadow-gray-400 
                       transition-all hover:shadow-lg hover:shadow-gray-400`}
     >
-      <Avatar className="w-28 h-28 sm:w-20 sm:h-20 md:w-28 md:h-28 lg:w-40 lg:h-40">
+      <Avatar className="w-28 h-28 sm:w-20 sm:h-20 md:w-28 md:h-28 lg:w-40 lg:h-40 border-2 border-black">
         <AvatarImage src={image_url} />
         <AvatarFallback>CN</AvatarFallback>
       </Avatar>
 
-      <div className="font-semibold w-full mt-4 mb-2 text-left">{name}</div>
+      <div className="font-semibold w-full mt-4 mb-2 text-left line-clamp-1">
+        {name}
+      </div>
       <div className="text-sm sm:text-base text-left line-clamp-2 w-full">
         {description}
       </div>
