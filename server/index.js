4d2a0fd06c7182194d346eae5195e17405567607
Saksham <113179290+Saksham131102@users.noreply.github.com>
2024-08-23 18:35:29 +0530

    connected frontend to server and database and made a fetch request to get investors data

--- server/index.js
+++ server/index.js
@@ -7,7 +7,12 @@ import db from "./db.js";
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(express.json());
 dotenv.config();
 
